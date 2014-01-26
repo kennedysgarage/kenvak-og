ecdbebb8de9f1670f5c0a4d6fde94e194800430b
Kennedy <kennedysgarage@gmail.com>
2014-01-26 00:06:40 -0500

    adding the form for new project

--- _assets/javascripts/script.js
+++ _assets/javascripts/script.js
@@ -3,7 +3,7 @@
 $(document).ready(function(){
 
 	// Contact form
-	$(".form-contact .btn").click( function() {
+	$("#form .btn").click( function() {
 	    $("#form").fadeOut(100);
 	    $("#contact-success").delay(100).fadeIn(1000);
 	});
