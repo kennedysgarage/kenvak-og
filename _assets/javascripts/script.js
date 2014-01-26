2f4eb01bdfab71ea51b352bd4c319b15eb126a39
Kennedy <kennedysgarage@gmail.com>
2014-01-25 23:26:02 -0500

    hooking up the contact form

--- _assets/javascripts/script.js
+++ _assets/javascripts/script.js
@@ -2,5 +2,10 @@
 
 $(document).ready(function(){
 
+	// Contact form
+	$(".form-contact .btn").click( function() {
+	    $("#form").fadeOut(100);
+	    $("#contact-success").delay(100).fadeIn(1000);
+	});
 
 });
